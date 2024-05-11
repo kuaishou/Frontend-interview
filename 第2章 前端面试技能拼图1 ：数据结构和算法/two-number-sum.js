0a5d065f05f827907d5285e711b3abd643c71853
邢浩东 <5487156+kuaishou1@user.noreply.gitee.com>
2024-05-11 12:12:28 +0800

    feat: add two-number-sum

--- 第2章 前端面试技能拼图1 ：数据结构和算法/two-number-sum.js
+++ 第2章 前端面试技能拼图1 ：数据结构和算法/two-number-sum.js
@@ -28,6 +28,36 @@ function findTwoNumber(arr, n) {//时间复杂度 n2---性能不好
 
 }
 
-const nArr=[1,5,8,9,12,17]
+const nArr = [1, 5, 8, 9, 12, 17]
 
-console.log('findTwoNumber',findTwoNumber(nArr,17))
+console.log('findTwoNumber', findTwoNumber(nArr, 17))
+
+//双指针实现方法
+
+function findTwoNumber1(arr, n) {
+
+    const res = []
+    const length = arr.length
+    if (length === 0) return res
+
+    let i = 0;
+    let j = length - 1;
+    while (i < j) {
+        const n1 = arr[i]
+        const n2 = arr[j]
+        const two = n1 + n2
+        if (two > n) {//和太大了，j减少
+            j--
+        } else if (two < n) {
+            i++
+        } else {
+            res.push(n1)
+            res.push(n2)
+            break
+        }
+    }
+    return res
+}
+
+
+console.log('findTwoNumber1', findTwoNumber1(nArr, 14))
